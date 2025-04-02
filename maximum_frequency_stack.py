701187680c5813adb3a7387f3189762b14a6c201
Marko Pasternak <pasternak.pn@ucu.edu.ua>
2025-04-02 11:03:13 +0300

    created all files

---
+++ maximum_frequency_stack.py
@@ -0,0 +1,27 @@
+"""Maximum Frequency Stack"""
+
+
+class FreqStack(object):
+    """Class implementing frequency stack"""
+
+    def __init__(self):
+        pass
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
